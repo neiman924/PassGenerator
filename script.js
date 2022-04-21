1e8edf4f5076521e2115f82d453146c93bbf43b7
Hossein Neiman <neiman924@gmail.com>
2022-04-20 21:04:52 -0700

    update 4-20 9:00pm

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-// Assignment Code
+//public variables 
 var generateBtn = document.querySelector("#generate");
-// var generateBtn = document.querySelector("#password");
 var passLenght = 0;
-var passatLeast;
 var passlowercase;
 var passupercase;
 var passNumber;
 var passspecial;
-var passgenerate = [];
 var charcap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var charlower = 'abcdefghijklmnopqrstuvwxyz';
 var numbers = '0123456789';
 var charsym = '~!@#$%^&*()_+-=:;}{,><./?';
 var password;
-var passwordText;
-//--------------------------------------------------
 
 
-// Write password to the #password input
-// function writePassword() {
-//   password = generatePassword();
-//   passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-//   alert(generatePassword());
-// }
-//--------------------------------------------------
-
-// Add event listener to generate button
+//this line was here when i arrived 
 generateBtn.addEventListener("click", collectInfo);
-//--------------------------------------------------
+
+
+//collection information from html and calling password generator function and
+//returning the result to html 
 function collectInfo(){
   passLenght = parseInt(document.getElementById("passLenght").value);
+  if (passLenght < 5 || passLenght > 128){
+    alert ("Password length should be between 5 and 128 characters");
+    location.reload();
+    return false;
+  }
   passlowercase = (document.getElementById("passlowercase").value);
   passupercase = (document.getElementById("passupercase").value);
   passNumber = (document.getElementById("passNumber").value);
@@ -38,18 +32,35 @@ function collectInfo(){
   password = generatePassword();
   document.getElementById('password').value = password;
 }
- //-------------------------------------------
 
 
-/////////////////////////////////////////////////
+ //password generator function , everytime loop runing its going to select new 
+ //element from characters variable with charAt and math.random function and
+ //adding it to the end for result variable 
 function generatePassword() {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-=:;}{,><./?';
+  var result = '';
+  var characters = setCharacters();
   var charactersLength = characters.length;
   for ( var i = 0; i < passLenght; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
  }
  return result;
 }
+ //specify character we want to have in the password and return it to generatepass function
+function setCharacters(){
+  var setChar = '';
+  if (passlowercase === 'true') setChar += 'abcdefghijklmnopqrstuvwxyz';
+  if (passupercase === 'true') setChar += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  if (passNumber === 'true') setChar += '0123456789';
+  if (passspecial === 'true') setChar += '~!@#$%^&*()_+-=:;}{,><./?';
+  //if user didnt select any of them:
+  if (passlowercase === 'false' && passupercase === 'false' && passspecial === 'false' && passNumber=== 'false' )
+{
+  alert("You didn't pick any specific characters so I picked a secure password for you");
+  setChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-=:;}{,><./?';
+}
 
+
+  return setChar;
+}
+ //-------------------------------------------
